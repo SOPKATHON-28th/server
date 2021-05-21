5a167b64fee961b933fcca5341ea85d69ebb8366
seu11ee <yslee2504@naver.com>
2021-05-21 23:54:55 +0900

    add image api

---
+++ src/api/image.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import {upload} from '../modules/upload';
+
+const router = express.Router();
+
+router.post("/", upload.single('img'),async(req,res)=>{
+    const image = (req as any).file.location;
+    res.send({
+        name: req.body.name,
+        imageUrl: image
+    });
+});
+
+module.exports = router;
